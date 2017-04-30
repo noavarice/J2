f525df5639912b112973c64254bdae2362317507
noavarice <alex9razinkov@gmail.com>
2017-04-30 13:13:10 +0300

    refactor: added method which converts user input to internal command
    This method supposed to interpret user input and make internal command
    which will be then executed by appropriate controller.

--- src/com/company/interaction/InteractionManager.java
+++ src/com/company/interaction/InteractionManager.java
@@ -1,5 +1,7 @@
 package com.company.interaction;
 
+import java.util.Hashtable;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InteractionManager {
@@ -24,4 +26,26 @@ public class InteractionManager {
         Pattern.compile("\\s*(delete)\\s+((" + PRIMARY_KEY + "\\s*(,\\s*" + PRIMARY_KEY + "\\s*)*)|\\*)"),
         Pattern.compile("\\s*(update)\\s+(" + PRIMARY_KEY + ")\\s+(" + SET_VALUES_STMT + ")")
     };
+
+    private static final Hashtable<String, CommandType> nameToCommandType = new Hashtable<String, CommandType>() {
+        {
+            put("insert", CommandType.INSERT);
+            put("delete", CommandType.DELETE);
+            put("update", CommandType.UPDATE);
+        }
+    };
+
+    public static TerminalCommand getCommandFromInput(String userInput) {
+        // прогоняем строку по регуляркам, если нашли совпадение - создаем из строки команду
+        for (int i = 0; i < COMMAND_PATTERNS.length; ++i) {
+            Matcher matcher = COMMAND_PATTERNS[i].matcher(userInput);
+            if (matcher.matches()) {
+                CommandType type = nameToCommandType.get(matcher.group(CommandPart.COMMAND_NAME.ordinal()));
+                int id = Integer.parseInt(matcher.group(CommandPart.PRIMARY_KEY.ordinal()));
+                String columnValuesPairs = matcher.group(CommandPart.SET_VALUES_STATEMENT.ordinal());
+                return new TerminalCommand(type, id, columnValuesPairs);
+            }
+        }
+        return new TerminalCommand();
+    }
 }
