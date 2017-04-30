b4b414738625f2187f3f72b1c9d98a9ed40b2e05
noavarice <alex9razinkov@gmail.com>
2017-04-30 12:17:43 +0300

    refactor: improved regexes, added enum to get captured string's parts
    Brief list of changes:
      - added CommandPart enum for more readable getting strings captured by
        main regex's subexpressions;
      - all main parts of command now enclosed by parethesis to get them
        directly;
      - column names/values replaced with its common representation
        accroding to MySQL documentation;
      - ArrayList of Patterns replaced with simple array.

--- src/com/company/interaction/InteractionManager.java
+++ src/com/company/interaction/InteractionManager.java
@@ -2,22 +2,27 @@ package com.company.interaction;
 
 import java.lang.String;
 import java.util.regex.Pattern;
-import java.util.ArrayList;
 
 public class InteractionManager {
-    private static final String COLUMN_NAMES = "";
+    private enum CommandPart {
+        COMMAND_NAME,
+        PRIMARY_KEY,
+        SET_VALUES_STATEMENT
+    };
 
-    private static final String SET_VALUE_STMT = COLUMN_NAMES + "\\s*=\\s*.+";
+    private static final String COLUMN_NAME_IDENTIFIER = "[\\w$]+";
+
+    private static final String COLUMN_VALUE_IDENTIFIER = "[\\w]*";
+
+    private static final String SET_VALUE_STMT = COLUMN_NAME_IDENTIFIER + "\\s*=\\s*" + COLUMN_VALUE_IDENTIFIER;
 
     private static final String SET_VALUES_STMT = SET_VALUE_STMT + "\\s*(,\\s*" + SET_VALUE_STMT + "\\s*)*";
 
     private static final String PRIMARY_KEY = "id\\s*=\\s*[1-9]\\d*";
 
-    private static final ArrayList<Pattern> COMMAND_PATTERNS = new ArrayList<Pattern>() {
-        {
-            Pattern.compile("\\s*insert\\s+" + SET_VALUES_STMT);
-            Pattern.compile("\\s*delete\\s+(" + PRIMARY_KEY + "\\s*(,\\s*" + PRIMARY_KEY + "\\s*)*)|\\*");
-            Pattern.compile("\\s*update\\s+" + PRIMARY_KEY + "\\s+" + SET_VALUES_STMT);
-        }
+    private static final Pattern[] COMMAND_PATTERNS = {
+        Pattern.compile("\\s*(insert)\\s+()(" + SET_VALUES_STMT + ")"),
+        Pattern.compile("\\s*(delete)\\s+((" + PRIMARY_KEY + "\\s*(,\\s*" + PRIMARY_KEY + "\\s*)*)|\\*)"),
+        Pattern.compile("\\s*(update)\\s+(" + PRIMARY_KEY + ")\\s+(" + SET_VALUES_STMT + ")")
     };
 }
