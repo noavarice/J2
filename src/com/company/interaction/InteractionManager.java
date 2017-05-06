0557238e7860144f7ec6a641b329713e9232a836
noavarice <alex9razinkov@gmail.com>
2017-05-06 23:29:37 +0300

    changed regular expressions for user input to more concrete

--- src/com/company/interaction/InteractionManager.java
+++ src/com/company/interaction/InteractionManager.java
@@ -11,20 +11,57 @@ public class InteractionManager {
         SET_VALUES_STATEMENT
     };
 
-    private static final String COLUMN_NAME_IDENTIFIER = "[\\w$]+";
+    private static final String MYSQL_COLUMN_NAME_IDENTIFIER = "[\\w$]+";
+
+    private static final String ASSIGNMENT_PATTERN = "\\s*=\\s*";
+
+    private static final String NOT_NULL_STRING_PATTERN = "\"[^\"]+\"";
+
+    private static final String NULL_STRING_PATTERN = "\"[^\"]*\"";
+
+    private static final String SET_NAME = "(\\bname" + ASSIGNMENT_PATTERN + NOT_NULL_STRING_PATTERN + ")";
+
+    private static final String SET_DESCRIPTION = "(\\bdescription" + ASSIGNMENT_PATTERN + NULL_STRING_PATTERN + ")";
+
+    private static final String ID_PATTERN = "(([01]?\\d{1,2})" +
+                                             "|(2[0-4]\\d)" +
+                                             "|(25[0-5]))";
+
+    private static final String SET_CATHEGORY_ID = "(\\bcathegory_id" + ASSIGNMENT_PATTERN + ID_PATTERN + ")";
+
+    private static final String PRICE_PATTERN = "(\\d{1,5}(\\.\\d{1,2})?)";
+
+    private static final String SET_PRICE = "(\\bprice" + ASSIGNMENT_PATTERN + PRICE_PATTERN + ")";
+
+    private static final String SET_BRAND = "(\\bbrand" + ASSIGNMENT_PATTERN + NOT_NULL_STRING_PATTERN + ")";
 
     private static final String COLUMN_VALUE_IDENTIFIER = "[\\w]*";
 
-    private static final String SET_VALUE_STMT = COLUMN_NAME_IDENTIFIER + "\\s*=\\s*" + COLUMN_VALUE_IDENTIFIER;
+    private static final String PRIMARY_KEY = "(\\bid" + ASSIGNMENT_PATTERN + ID_PATTERN + ")";
 
-    private static final String SET_VALUES_STMT = SET_VALUE_STMT + "\\s*(,\\s*" + SET_VALUE_STMT + "\\s*)*";
+    private static final String PRIMARY_KEYS = PRIMARY_KEY + "\\s*(,\\s*" + PRIMARY_KEY + "\\*)";
 
-    private static final String PRIMARY_KEY = "id\\s*=\\s*[1-9]\\d*";
+    private static final String CATHEGORY_COMMAND_PREFIX = "\\bcathegory\\b";
+
+    private static final String GOODS_COMMAND_PREFIX = "\\bgoods\\b";
+
+    private static final String INSERT_COMMAND = "\\binsert\\b";
+
+    private static final String DELETE_COMMAND = "\\bdelete\\b";
+
+    private static final String UPDATE_COMMAND = "\\bupdate\\b";
 
     private static final Pattern[] COMMAND_PATTERNS = {
-        Pattern.compile("\\s*(insert)\\s+()(" + SET_VALUES_STMT + ")"),
-        Pattern.compile("\\s*(delete)\\s+((" + PRIMARY_KEY + "\\s*(,\\s*" + PRIMARY_KEY + "\\s*)*)|\\*)"),
-        Pattern.compile("\\s*(update)\\s+(" + PRIMARY_KEY + ")\\s+(" + SET_VALUES_STMT + ")")
+        Pattern.compile("\\s*" + CATHEGORY_COMMAND_PREFIX + "\\s+" + INSERT_COMMAND + "\\s+" + SET_NAME + "\\s+" +
+                        SET_DESCRIPTION + "\\s*"),
+        Pattern.compile("\\s*" + GOODS_COMMAND_PREFIX + "\\s+" + INSERT_COMMAND + "\\s+" + SET_CATHEGORY_ID + "\\s+" +
+                        SET_PRICE + "\\s+" + SET_BRAND + "\\s*"),
+        Pattern.compile("\\s*" + CATHEGORY_COMMAND_PREFIX + "\\s+" + DELETE_COMMAND + "\\s+" + PRIMARY_KEYS),
+        Pattern.compile("\\s*" + GOODS_COMMAND_PREFIX + "\\s+" + DELETE_COMMAND + "\\s+" + PRIMARY_KEYS),
+        Pattern.compile("\\s*" + CATHEGORY_COMMAND_PREFIX + "\\s+" + UPDATE_COMMAND + "\\s+" + "(" + SET_NAME +
+                        "|" + SET_DESCRIPTION + ")\\s*"),
+        Pattern.compile("\\s*" + GOODS_COMMAND_PREFIX + "\\s+" + UPDATE_COMMAND + "\\s+" + "(" + SET_CATHEGORY_ID +
+                        "|" + SET_PRICE + "|" + SET_BRAND + ")\\s*")
     };
 
     private static final Hashtable<String, CommandType> nameToCommandType = new Hashtable<String, CommandType>() {
