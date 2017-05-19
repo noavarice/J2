da38902a73073b5b93aa006e7c8133762617d429
noavarice <alex9razinkov@gmail.com>
2017-05-19 23:19:31 +0300

    changed to satisfy implementation of IController

--- src/com/company/controllers/DatabaseController.java
+++ src/com/company/controllers/DatabaseController.java
@@ -1,14 +1,12 @@
 package com.company.controllers;
 
-import com.company.interaction.TerminalCommand;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
-public class DatabaseController extends AbstractController {
+public class DatabaseController implements IController {
     private static final String INSERT_STMT = "INSERT INTO %1 (%2) VALUES (%3)";
 
     private static final String DELETE_STMT = "DELETE FROM %1 WHERE id = %2";
@@ -31,7 +29,7 @@ public class DatabaseController extends AbstractController {
                                                       IOException,
                                                       SQLException
     {
-        Properties props = getConnectionProperties(filePath);
+        /*Properties props = getConnectionProperties(filePath);
         Class.forName(props.getProperty("driver")).newInstance();
         StringBuilder builder = new StringBuilder();
         builder.append(props.getProperty("url"))
@@ -39,33 +37,10 @@ public class DatabaseController extends AbstractController {
                .append(props.getProperty("username"))
                .append("&password=")
                .append(props.getProperty("password"));
-        connection = DriverManager.getConnection(builder.toString());
+        connection = DriverManager.getConnection(builder.toString());*/
     }
 
-    public final boolean tryExecute(TerminalCommand command)
-    {
-        tableName = command.getTableName();
-        switch (command.getCommandType()) {
-            case NOT_VALID: return false;
-
-            case SHOW: show();
-                break;
-
-            case INSERT: {
-                Properties props = new Properties();
-                String[] pairs = command.getColumnValues().split(",");
-                for (int i = 0; i < pairs.length; ++i) {
-                    String[] pair = pairs[i].split("=");
-                    props.put(pair[0].trim(), pair[1].trim());
-                }
-                insert(props);
-            }
-            break;
-        }
-        return true;
-    }
-
-    protected final void insert(Properties props)
+    public void insert(Properties props)
     {
         StringBuilder columnNamesBuilder = new StringBuilder();
         StringBuilder columnValuesBuilder = new StringBuilder();
@@ -75,15 +50,15 @@ public class DatabaseController extends AbstractController {
         }
     }
 
-    protected final void delete(int id)
+    public void delete(int id)
     {
     }
 
-    protected final void update(int id, Properties props)
+    public void update(int id, Properties props)
     {
     }
 
-    protected final void show()
+    public void show()
     {
     }
 }
