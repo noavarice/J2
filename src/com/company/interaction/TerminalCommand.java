4319681f59436ef49f97a1aea747aa0c264ccd8d
noavarice <alex9razinkov@gmail.com>
2017-05-19 23:23:04 +0300

    CommandType removed from TerminalCommand

--- src/com/company/interaction/TerminalCommand.java
+++ src/com/company/interaction/TerminalCommand.java
@@ -1,17 +1,7 @@
 package com.company.interaction;
 
-enum CommandType {
-    NOT_VALID,
-    INSERT,
-    DELETE,
-    UPDATE,
-    SHOW,
-}
-
 public class TerminalCommand {
     private String tableName;
-
-    private CommandType comType = CommandType.NOT_VALID;
 
     private int id = -1;
 
@@ -21,14 +11,11 @@ public class TerminalCommand {
 
     public TerminalCommand(String tableName, CommandType type, int id, String columnValuePairs) {
         this.tableName = tableName;
-        comType = type;
         this.id = id;
         columnValues = columnValuePairs;
     }
 
     public String getTableName() { return tableName; }
-
-    public CommandType getCommandType() { return comType; }
 
     public int getId() {
         return id;
