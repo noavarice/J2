fa2fcce299764dc123814ec298f27b253ea3fded
noavarice <alex9razinkov@gmail.com>
2017-05-28 22:51:14 +0300

    added declaration for ProductLoader class

---
+++ src/loader/ProductLoader.java
@@ -0,0 +1,15 @@
+package loader;
+
+import com.company.models.Product;
+
+import java.util.LinkedList;
+
+public class ProductLoader {
+    public static LinkedList<Product> loadFromDatabase(String connectionPropsFilePath)
+    {
+    }
+
+    public static LinkedList<Product> loadFromFile(String filePath)
+    {
+    }
+}
