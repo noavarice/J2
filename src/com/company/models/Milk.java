030434826453bfa62a9db43a39f6992fcb658906
noavarice <alex9razinkov@gmail.com>
2017-05-28 21:06:27 +0300

    added milk product model

---
+++ src/com/company/models/Milk.java
@@ -0,0 +1,23 @@
+package com.company.models;
+
+public class Milk extends Product {
+    private double fattiness;
+
+    private String brand;
+
+    public Milk(double price, double fattiness, String brand)
+    {
+        super(price);
+        this.fattiness = fattiness;
+        this.brand = brand;
+    }
+
+    public String toString()
+    {
+        StringBuilder b = new StringBuilder();
+        b.append("Milk, price: ").append(String.valueOf(price))
+                .append("fattiness: ").append(String.valueOf(fattiness))
+                .append("brand: ").append(brand);
+        return b.toString();
+    }
+}
