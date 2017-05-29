87ddf01f9be484477fa19694121227eb2569a67d
noavarice <alex9razinkov@gmail.com>
2017-05-29 11:48:40 +0300

    defined loadFromDatabase method

--- src/com/company/loader/ProductLoader.java
+++ src/com/company/loader/ProductLoader.java
@@ -1,12 +1,57 @@
 package com.company.loader;
 
+import com.company.models.Bread;
+import com.company.models.Meat;
+import com.company.models.Milk;
 import com.company.models.Product;
 
+import java.sql.*;
+import java.util.Hashtable;
 import java.util.LinkedList;
 
+enum ProductType {
+    Bread,
+    Meat,
+    Milk
+}
+
 public class ProductLoader {
-    public static LinkedList<Product> loadFromDatabase(String connectionPropsFilePath)
+    private static Hashtable<String, ProductType> nameToType = new Hashtable<String, ProductType>() {
+        {
+            put("bread", ProductType.Bread);
+            put("meat", ProductType.Meat);
+            put("milk", ProductType.Milk);
+        }
+    };
+
+    public static LinkedList<Product> loadFromDatabase(Connection databaseConnection) throws
+            SQLException
     {
+        LinkedList<Product> result = new LinkedList<>();
+        Statement s = databaseConnection.createStatement();
+        ResultSet rs = s.executeQuery("SELECT * FROM products");
+        while (rs.next()) {
+            Product temp = null;
+            double price = rs.getDouble("price");
+            switch (nameToType.get(rs.getString("product_name"))) {
+                case Bread: {
+                    temp = new Bread(price, rs.getString("flour_type"));
+                }
+                break;
+
+                case Meat: {
+                    temp = new Meat(price, rs.getString("meat_type"));
+                }
+                break;
+
+                case Milk: {
+                    temp = new Milk(price, rs.getDouble("fattiness"), rs.getString("brand"));
+                }
+                break;
+            }
+            result.add(temp);
+        }
+        return result;
     }
 
     public static LinkedList<Product> loadFromFile(String filePath)
